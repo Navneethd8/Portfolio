6d9acb418781084d4f99a60b24769ffa4f782062
Navneeth Dhamotharan <navneethdhamotharan@gmail.com>
2025-08-13 21:37:16 +0400

    data projects + swe projects scaffolding

--- navneeth-portfolio/src/app/page.tsx
+++ navneeth-portfolio/src/app/page.tsx
@@ -224,8 +224,8 @@ export default function Home() {
             </div>
 
             <div className="flex justify-between flex-wrap">
-              <span className="text-[var(--h3)]">FlyDubai</span>
-              <span className="text-[var(--h3)]">Dubai, UAE</span>
+              <span className="text-[var(--h3)]">Software Engineering Career Club</span>
+              <span className="text-[var(--h3)]">Seattle, WA</span>
             </div>
 
             <ul className="list-disc pl-5 mt-2 space-y-1 text-[var(--h4)]">
@@ -385,11 +385,12 @@ export default function Home() {
         <h1 id="skills-subheading" className="relative text-2xl font-bold mb-4 after:content-[''] after:block after:h-[3px] after:w-16 after:bg-[var(--foreground)] after:mt-2">
           Projects
         </h1>
+        
         <div className="project-card border-b border-gray-300 pb-4 mb-6">
           <div className="flex">
             <div className="flex-1 pr-4">
               <div className="flex justify-between items-center mb-1">
-                <h3 className="text-lg font-bold">Project Name</h3>
+                <h3 className="text-lg font-bold">SAT Decoded</h3>
                 <div className="text-lg whitespace-nowrap">
                   Jan. 2024 – Present
                 </div>
@@ -408,7 +409,136 @@ export default function Home() {
               </ul>
             </div>
 
-            {/* Right 40% */}
+            <div className="w-2/5 flex items-start justify-end">
+              <img
+                src="images/ETLogo.png"
+                alt="Project Screenshot"
+                className="w-full h-auto max-h-40 object-cover rounded-md"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="project-card border-b border-gray-300 pb-4 mb-6">
+          <div className="flex">
+            <div className="flex-1 pr-4">
+              <div className="flex justify-between items-center mb-1">
+                <h3 className="text-lg font-bold">Discord Bot</h3>
+                <div className="text-lg whitespace-nowrap">
+                  Jan. 2024 – Present
+                </div>
+              </div>
+
+              <div className="flex flex-wrap gap-2 mb-2">
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">React</span>
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Firebase</span>
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Tailwind</span>
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Plaid API</span>
+              </div>
+              <ul className="list-disc pl-5 mt-2 space-y-1 text-[var(--h4)]">
+                <li>Developed a budgeting and expense tracking feature integrated with Plaid API.</li>
+                <li>Implemented authentication and real-time database updates using Firebase.</li>
+                <li>Designed mobile-friendly UI using TailwindCSS with smooth animations.</li>
+              </ul>
+            </div>
+
+            <div className="w-2/5 flex items-start justify-end">
+              <img
+                src="images/ETLogo.png"
+                alt="Project Screenshot"
+                className="w-full h-auto max-h-40 object-cover rounded-md"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="project-card border-b border-gray-300 pb-4 mb-6">
+          <div className="flex">
+            <div className="flex-1 pr-4">
+              <div className="flex justify-between items-center mb-1">
+                <h3 className="text-lg font-bold">Bingo on the Ave</h3>
+                <div className="text-lg whitespace-nowrap">
+                  Jan. 2024 – Present
+                </div>
+              </div>
+
+              <div className="flex flex-wrap gap-2 mb-2">
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">React</span>
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Firebase</span>
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Tailwind</span>
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Plaid API</span>
+              </div>
+              <ul className="list-disc pl-5 mt-2 space-y-1 text-[var(--h4)]">
+                <li>Developed a budgeting and expense tracking feature integrated with Plaid API.</li>
+                <li>Implemented authentication and real-time database updates using Firebase.</li>
+                <li>Designed mobile-friendly UI using TailwindCSS with smooth animations.</li>
+              </ul>
+            </div>
+
+            <div className="w-2/5 flex items-start justify-end">
+              <img
+                src="images/ETLogo.png"
+                alt="Project Screenshot"
+                className="w-full h-auto max-h-40 object-cover rounded-md"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="project-card border-b border-gray-300 pb-4 mb-6">
+          <div className="flex">
+            <div className="flex-1 pr-4">
+              <div className="flex justify-between items-center mb-1">
+                <h3 className="text-lg font-bold">Mentura</h3>
+                <div className="text-lg whitespace-nowrap">
+                  Jan. 2024 – Present
+                </div>
+              </div>
+
+              <div className="flex flex-wrap gap-2 mb-2">
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">React</span>
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Firebase</span>
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Tailwind</span>
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Plaid API</span>
+              </div>
+              <ul className="list-disc pl-5 mt-2 space-y-1 text-[var(--h4)]">
+                <li>Developed a budgeting and expense tracking feature integrated with Plaid API.</li>
+                <li>Implemented authentication and real-time database updates using Firebase.</li>
+                <li>Designed mobile-friendly UI using TailwindCSS with smooth animations.</li>
+              </ul>
+            </div>
+
+            <div className="w-2/5 flex items-start justify-end">
+              <img
+                src="images/ETLogo.png"
+                alt="Project Screenshot"
+                className="w-full h-auto max-h-40 object-cover rounded-md"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="project-card border-b border-gray-300 pb-4 mb-6">
+          <div className="flex">
+            <div className="flex-1 pr-4">
+              <div className="flex justify-between items-center mb-1">
+                <h3 className="text-lg font-bold">Fundit</h3>
+                <div className="text-lg whitespace-nowrap">
+                  Jan. 2024 – Present
+                </div>
+              </div>
+
+              <div className="flex flex-wrap gap-2 mb-2">
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">React</span>
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Firebase</span>
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Tailwind</span>
+                <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Plaid API</span>
+              </div>
+              <ul className="list-disc pl-5 mt-2 space-y-1 text-[var(--h4)]">
+                <li>Developed a budgeting and expense tracking feature integrated with Plaid API.</li>
+                <li>Implemented authentication and real-time database updates using Firebase.</li>
+                <li>Designed mobile-friendly UI using TailwindCSS with smooth animations.</li>
+              </ul>
+            </div>
+
             <div className="w-2/5 flex items-start justify-end">
               <img
                 src="images/ETLogo.png"
@@ -420,40 +550,113 @@ export default function Home() {
         </div>
 
 
+
         <div className="project-card border-b border-gray-300 pb-4 mb-6">
           <div className="flex justify-between items-center mb-1">
-            <h3 className="text-lg font-bold">Project Name</h3>
-            <div className="text-lg">Jan. 2024 – Present</div>
+            <h3 className="text-lg font-bold">Stock Market Analysis</h3>
+            <div className="text-lg">Ongoing</div>
           </div>
 
           <div className="flex flex-wrap gap-2 mb-2">
-            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">React</span>
-            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Firebase</span>
-            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Tailwind</span>
-            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Plaid API</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Python</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">XGBoost</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Scikit-learn</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Pandas</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Tableau</span>
+
           </div>
 
           <ul className="list-disc pl-5 mt-2 space-y-1 text-[var(--h4)]">
-            <li>Developed a budgeting and expense tracking feature integrated with Plaid API.</li>
-            <li>Implemented authentication and real-time database updates using Firebase.</li>
-            <li>Designed mobile-friendly UI using TailwindCSS with smooth animations.</li>
+            <li>Developing a predictive model using the last 10 years of SPY data for efficiency.</li>
+            <li>Extending the model for real-time predictions via a BI dashboard and live data stream.</li>
           </ul>
         </div>
 
 
+        <div className="project-card border-b border-gray-300 pb-4 mb-6">
+          <div className="flex justify-between items-center mb-1">
+            <h3 className="text-lg font-bold">MacroScope</h3>
+            <div className="text-lg">May 2025. - Jun. 2025</div>
+          </div>
+
+          <div className="flex flex-wrap gap-2 mb-2">
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Python</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Geopandas</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Folium</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">XGBoost</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Scikit-learn</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Pandas</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Seaborn</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Matplotlib</span>
+          </div>
+
+          <ul className="list-disc pl-5 mt-2 space-y-1 text-[var(--h4)]">
+            <li>Processed 400K+ IMF records with Pandas/GeoPandas, integrating fiscal and balance-of-payments data, and built an interactive global economic map using Folium.</li>
+            <li>Generated 6 trend charts with Seaborn/Matplotlib to visualize macroeconomic indicators and support inflation prediction insights.</li>
+            <li>Improved prediction accuracy by 20%, reducing RMSE from 2.899 to 2.309 and maintaining MAE under 1.21 through Random Forest optimization, hyperparameter tuning, and data validation.</li>
+          </ul>
+        </div>
+
+
+
+        <div className="project-card border-b border-gray-300 pb-4 mb-6">
+          <div className="flex justify-between items-center mb-1">
+            <h3 className="text-lg font-bold">DataMed</h3>
+            <div className="text-lg">Apr. 2025</div>
+          </div>
+
+          <div className="flex flex-wrap gap-2 mb-2">
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Python</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Scikit-learn</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Optuna</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Pandas</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Numpy</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Seaborn</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">Matplotlib</span>
+          </div>
+
+          <ul className="list-disc pl-5 mt-2 space-y-1 text-[var(--h4)]">
+            <li>Awarded <b>3rd</b> place for <b>Best Machine Learning Model</b> in the 6th DubsTech Datathon.</li>
+            <li>Predicted avgerage costs for <b>1.2M healthcare records</b> using Pandas and Scikit-learn, achieving 99% data retention.</li>
+            <li>Engineered features with Scikit-learn’s LabelEncoder, transforming 4 categorical variables for model readiness.</li>
+            <li>Tuned hyperparameters for predictive modeling using Scikit-learn and Optuna, improving prediction accuracy.</li>
+          </ul>
+        </div>
+
+        <div className="project-card border-b border-gray-300 pb-4 mb-6">
+          <div className="flex justify-between items-center mb-1">
+            <h3 className="text-lg font-bold">March Madness vs. Regular Season</h3>
+            <div className="text-lg">Jan 2025. - Mar. 2025</div>
+          </div>
+
+          <div className="flex flex-wrap gap-2 mb-2">
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">R</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">R Studio</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">tidyverse</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">ggplot2</span>
+            <span className="bg-[var(--link-hover)] dark:bg-[var(--link)] px-2 py-1 rounded-md text-xs font-mono">dplyr</span>
+          </div>
+
+          <ul className="list-disc pl-5 mt-2 space-y-1 text-[var(--h4)]">
+            <li>Analyzed 20 years of men's college basketball free throws across 300K+ rows from three datasets.</li>
+            <li>Calculated 0.25 correlation between regular season free throws and NCAA tournament wins.</li>
+            <li>Merged datasets, created 10+ analysis columns, cleaned missing values, and visualized success rates.</li>
+          </ul>
+        </div>
+
       </section>
       <section id="resume" className="mb-4">
-        <h1 id="skills-subheading" className="relative text-2xl font-bold mb-4 after:content-[''] after:block after:h-[3px] after:w-16 after:bg-[var(--foreground)] after:mt-2">
+        <h1 id="skills-subheading" className="relative text-2xl font-bold mb-6 after:content-[''] after:block after:h-[3px] after:w-16 after:bg-[var(--foreground)] after:mt-2">
           Resume
         </h1>
-        <h2 className="text-xl font-semibold mb-1 text-start md:text-center">
-          Download my resume below
+        <h2 className="text-xl font-semibold mb-6 text-center">
+          Download my resume below!
         </h2>
-        <div className="flex justify-start md:justify-center">
+        <div className="flex justify-center">
           <a
             href="Resume_Navneeth.pdf"
             target="_blank"
-            className="resume-link px-3 py-1 rounded-md text-sm font-mono text-center"
+            className="resume-link px-6 py-2 rounded-md text-lg font-mono text-center"
           >
             Resume
           </a>
