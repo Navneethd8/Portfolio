a6a8fedfe2bdc300661e984a0d22c626346d2226
Navneeth Dhamotharan <navneethdhamotharan@gmail.com>
2026-02-26 18:44:39 -0800

    typewriter text in large screens

---
+++ src/components/TypewriterText.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+const titles = [
+    "Software Engineer",
+    "Data Engineer",
+    "Machine Learning Engineer",
+    "Data Scientist",
+];
+
+export default function TypewriterText() {
+    const [titleIndex, setTitleIndex] = useState(0);
+    const [charIndex, setCharIndex] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    useEffect(() => {
+        const currentTitle = titles[titleIndex];
+
+        const timeout = setTimeout(() => {
+            if (!isDeleting) {
+                // Typing
+                if (charIndex < currentTitle.length) {
+                    setCharIndex(charIndex + 1);
+                } else {
+                    // Pause at end before deleting
+                    setTimeout(() => setIsDeleting(true), 1500);
+                }
+            } else {
+                // Deleting
+                if (charIndex > 0) {
+                    setCharIndex(charIndex - 1);
+                } else {
+                    setIsDeleting(false);
+                    setTitleIndex((titleIndex + 1) % titles.length);
+                }
+            }
+        }, isDeleting ? 40 : 80);
+
+        return () => clearTimeout(timeout);
+    }, [charIndex, isDeleting, titleIndex]);
+
+    const displayText = titles[titleIndex].slice(0, charIndex);
+
+    return (
+        <span>
+            Aspiring {displayText}
+            <span className="animate-pulse">|</span>
+        </span>
+    );
+}
